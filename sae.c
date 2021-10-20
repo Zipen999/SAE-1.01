206168d63e038503aba283b3dbd7cf6b59115115
fbschucky <92808098+fbschucky@users.noreply.github.com>
2021-10-20 16:23:26 +0200

    Update sae.c

--- sae.c
+++ sae.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "sae.h"
+void Saisie (void)
+{
+	int i,n;
+	printf("combien d'article : ");
+	scanf("%d", &n );
+	int ref[n],qt [n], sds [n];
+	float prix[n];
+ 	for (i=0; i<n; i++)
+    	{
+     		printf("reference %d : ", i);
+     		scanf("%d", &ref[i]);
+     		printf("quantité %d : ", i);
+     		scanf("%d", &qt[i]);
+		printf("Prix %d : ", i);
+     		scanf("%f", &prix[i]);
+     		printf("seuil de sécurité %d : ", i);
+     		scanf("%d", &sds[i]);
+
+	}
+	FILE * flot;
+	flot=fopen("nom.txt","a");
+	if (flot == NULL)
+	{
+		printf("probleme d'ouverture du fichier \n");
+		exit(1);
+	}
+	for (i=0; i<n; i++)
+		fprintf(flot,"%d\t%d\t%.2f\t%d\n",ref[i],qt[i],prix[i],sds[i]);
+
+	fclose(flot);
+}
 
 void fEtatStock(void)
 {
