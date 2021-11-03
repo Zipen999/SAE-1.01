bf9e47fa951b6c8a4b90cf99d627c5ddb955df9a
Zipen999 <78689752+Zipen999@users.noreply.github.com>
2021-11-03 12:13:36 +0100

    Add files via upload

---
+++ SAE1.01/sae.h
@@ -0,0 +1,12 @@
+void globale (void);
+void fCreerStock (void);
+void fEtatStock(int ref[],int qt[],float prix[],int sds[],int n);
+int fConsulterStock (int ref[],int qt[],float prix[],int sds[],int n);
+void fAfficherStock (int ref[],int qt[],float prix[],int sds[],int n);
+int NbArticle (void);
+void fVerif(int tab[],int i);
+void fVerifReal(float tab[],int i);
+void fAppro (int ref[],int qt[],float prix[],int sds[],int n);
+int fRecherche (int ref[],int n,int nref);
+int fEnreg(int ref[],int qt[],float prix[],int sds[],int n);
+int fSuppression (int ref[],int qt[],float prix[],int sds[],int n);
